962b3b0bb1a973f2f98a36199e021aa4ba139789
Tom Schenk Jr <tomschenkjr@gmail.com>
2013-05-14 14:39:33 -0500

    Updated connection info.

--- include/dbconnopen.php
+++ include/dbconnopen.php
@@ -1,7 +1,7 @@
 <?php
 
 //production database connection
-$cnnCDD = mysqli_connect("mysql.tomschenkjr.net", "tomschenkjrnet", "Str0ngp***word", "datadictionary")
+$cnnCDD = mysqli_connect("yourlocaldatabase", "db-user", "db-password", "datadictionary")
                     or die ("Error Connecting To The Database Because: " . mysqli_connect_error());
 
 ?>
