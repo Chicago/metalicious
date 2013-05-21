581265074971e29b1ef4308436c134f5bd36b8de
tomschenkjr <tomschenkjr@gmail.com>
2013-05-21 05:10:40 +0000

    Added relative paths

--- search.php
+++ search.php
@@ -1,16 +1,16 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/classes/database.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/classes/table.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/classes/variable.php';
+include 'classes/database.php';
+include 'classes/table.php';
+include 'classes/variable.php';
 
-include $_SERVER['DOCUMENT_ROOT'] . '/include/header.php';
+include 'include/header.php';
 
 //open DB
-include $_SERVER['DOCUMENT_ROOT'] . '/include/dbconnopen.php';
+include 'include/dbconnopen.php';
 $search_results = mysqli_query($cnnCDD, "Call Main_Search('" . $_POST['search_criteria'] . "')");
 
 //close DB
-include ($_SERVER['DOCUMENT_ROOT'] . '/include/dbconnclose.php');
+include ('include/dbconnclose.php');
 ?>
 <script>
     $(document).ready(function() {
@@ -19,7 +19,7 @@ include ($_SERVER['DOCUMENT_ROOT'] . '/include/dbconnclose.php');
 </script>
 
 <h3>Search Results:</h3>
-<form action="/search.php" method="post">
+<form action="search.php" method="post">
     <input type="text" id="search_criteria" class="main-search span5" name="search_criteria" style="margin: 0px;" value="<?php echo (isset($_POST['search_criteria'])) ? $_POST['search_criteria'] : ''; ?>" />
     <input type="submit" class="btn btn-primary" value="Search" />
 </form>
@@ -31,21 +31,21 @@ include ($_SERVER['DOCUMENT_ROOT'] . '/include/dbconnclose.php');
         if ($result_element['Element_Type'] == 'Database') {
             $database = Database::get_database_info($result_element['Element_ID']);
             ?>
-            <a href="/database_info.php?database_id=<?php echo $database['Database_ID']; ?>"><i class="icon-th-large"></i> <?php echo $database['Database_Name']; ?></a> (database)<br />
+            <a href="database_info.php?database_id=<?php echo $database['Database_ID']; ?>"><i class="icon-th-large"></i> <?php echo $database['Database_Name']; ?></a> (database)<br />
             <?php
         } else if ($result_element['Element_Type'] == 'Table') {
             $table = Table::get_table_info($result_element['Element_ID']);
             ?>
-            <a href="/table_info.php?table_id=<?php echo $table['Table_ID']; ?>"><i class="icon-th"></i> <?php echo $table['Table_Name']; ?></a> (table)<br />
+            <a href="table_info.php?table_id=<?php echo $table['Table_ID']; ?>"><i class="icon-th"></i> <?php echo $table['Table_Name']; ?></a> (table)<br />
             <?php
         } else if ($result_element['Element_Type'] == 'Variable') {
             $variable = Variable::get_variable_info($result_element['Element_ID']);
             ?>
-            <a href="/variable_info.php?variable_id=<?php echo $variable['Variable_ID']; ?>"><i class="icon-asterisk"></i> <?php echo $variable['Variable_Name']; ?></a> (variable)<br />
+            <a href="variable_info.php?variable_id=<?php echo $variable['Variable_ID']; ?>"><i class="icon-asterisk"></i> <?php echo $variable['Variable_Name']; ?></a> (variable)<br />
             <?php
         }
     }
     ?>
 </div>
 
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'; ?>
+<?php include 'include/footer.php'; ?>
